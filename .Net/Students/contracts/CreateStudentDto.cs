4aed70d6db3752a33ec7d67db8d845536141d527
mohamedhassan218 <m.hassan.def@gmail.com>
2024-06-25 09:45:07 +0300

    . . .

--- .Net/Students/contracts/CreateStudentDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Students.contrcts;
-
-public record class CreateStudentDto(
-    [Required][StringLength(30)] string Name,
-    [Required][StringLength(6)] string Gender,
-    [Required] DateOnly BirthDate
-);
--- .Net/Students/contracts/StudentDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Students.contrcts;
-
-public record class StudentDto(
-    int Id,
-    string Name,
-    string Gender,
-    DateOnly BirthDate
-);
